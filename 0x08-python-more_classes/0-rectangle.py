2edff9731deaff266bf7467b79a5f6638b82cd7e
Mawusi Kpodo <60935336+mawusikpodo@users.noreply.github.com>
2023-07-03 20:39:14 +0000

    update

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+class Rectangle
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+
+    Attributes:
+        empty
+    """
+
+    pass
